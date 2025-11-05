c25ada7b33d522a3722a6c0380f939c95eb39b89
Kattemat <tuvakleberg@gmail.com>
2025-11-05 12:44:09 +0100

    start of key generation tests

--- tests/Fhi.Authentication.Extensions.UnitTests/Tokens/JwkGeneratorTests.cs
+++ tests/Fhi.Authentication.Extensions.UnitTests/Tokens/JwkGeneratorTests.cs
@@ -5,7 +5,28 @@ namespace Fhi.Authentication.Extensions.UnitTests.Tokens
     public class JwkGeneratorTests
     {
         [Test]
-        public void GenerateJwk()
+        public void GIVEN_GenerateJwk_WHEN_OnlyDefaultValues_THEN_CreateValidKeyPair()
+        {
+            var jwk = JwkGenerator.GenerateRsaJwk();
+            Console.WriteLine(jwk);
+        }
+
+        [Test]
+        public void GIVEN_GenerateJwk_WHEN_CustomKidValue_THEN_CreateValidKeyPairWithCustomKid()
+        {
+            var jwk = JwkGenerator.GenerateRsaJwk();
+            Console.WriteLine(jwk);
+        }
+
+        [Test]
+        public void GIVEN_GenerateJwk_WHEN_InvalidKeyUseType_THEN_ReturnEmptyKeyPair()
+        {
+            var jwk = JwkGenerator.GenerateRsaJwk();
+            Console.WriteLine(jwk);
+        }
+
+        [Test]
+        public void GIVEN_GenerateJwk_WHEN_InvalidSigningAlgorithm_THEN_ReturnEmptyKeyPair()
         {
             var jwk = JwkGenerator.GenerateRsaJwk();
             Console.WriteLine(jwk);
