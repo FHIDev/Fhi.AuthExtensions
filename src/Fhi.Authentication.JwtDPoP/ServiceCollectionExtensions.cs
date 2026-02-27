b758eb81ed070d082080efdbca18918784b0e7bf
Jenny Beate Hougen <Jenny.Beate.Hougen@fhi.no>
2026-02-27 08:05:26 +0100

    added project

---
+++ src/Fhi.Authentication.JwtDPoP/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Fhi.Authentication.JwtDPoP
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds JWT DPoP (Demonstration Proof of Possession) authentication support to the specified authentication
+        /// builder.
+        /// </summary>
+        /// <param name="builder">The authentication builder to which JWT DPoP support will be added.</param>
+        /// <returns>The updated authentication builder with JWT DPoP support configured.</returns>
+        public static AuthenticationBuilder AddJwtDpop(this AuthenticationBuilder builder)
+        {
+            return builder;
+        }
+    }
+}
